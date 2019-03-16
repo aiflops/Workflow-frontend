4b7333ce106dc12fe0c46f54bb190dafb2b2f0cf
Jasiek <k.kalfas@netkoncept.com>
2019-03-16 10:58:30 +0100

    livio zadanie

--- src/app/dashboard/extend/extend.component.ts
+++ src/app/dashboard/extend/extend.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+
+declare var moment;
 
 @Component({
   selector: 'app-extend',
@@ -8,11 +11,42 @@ import { Component, OnInit } from '@angular/core';
 export class ExtendComponent implements OnInit {
 
 
-  public extendPerson = "";
+  public errorRequired = { id: false, startExtend: false, endExtend: false };
+  //  zmienna error
+  public error = false;
 
-  constructor() { }
+  public extendPerson = '';
+  public usersList = new Array<any>();
+
+
+  constructor(private api: ApiService) {
+    this.api.getUsers().subscribe(res => {
+      this.usersList  = res.data.map( item => {
+        return {id: item.id, email: item.email, name: item.firstName + ' ' + item.lastName};
+      });
+      console.log(this.usersList);
+    });
+   }
 
   ngOnInit() {
   }
+
+  submit(form) {
+    const formControls = form['form']['controls'];
+    for (let key in formControls) {
+      if (!!formControls[key].errors) {
+        this.errorRequired[key] = true;
+      } else {
+        this.errorRequired[key] = false;
+      }
+    }
+    // moment JS poronanie dat
+    console.log(form.value['startExtend'], form.value['endExtend']);
+    if (form.valid) {
+      // console.log(form.value['startExtend'], form.value['endExtend'] );
+    }
+  }
+
+
 
 }
